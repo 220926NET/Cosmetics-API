a6ae930718e05847c3c17e788939122e6c3fcc5e
HaizhenHuang <hhuang56@ucmerced.edu>
2022-11-21 12:22:49 -0800

    lipstick api data

--- ECommerce.API/CosmeticSeedDataGroupFile.sql
+++ ECommerce.API/CosmeticSeedDataGroupFile.sql
@@ -1,6 +1,62 @@
+/*
 USE [Reimbursement];
 GO
+*/
 
 set identity_insert [Products] on
-insert into [Products] ([ProductID],[API_ID],[ProductName],[Brand],[Inventory],[Price], [Description],[Image],[ColourName],[HexValue]) values (,309,'Maybelline Expert Wear Eye Shadow Quad ','maybelline',20,8.99, 'Easy to use, lots to choose!Maybelline Expert Wear Eye Shadow \nQuads have 4 coordinating shades with step by step application guide \nmakes shadow easier than ever. The eyeshadows glide on effortlessly with\n superior smoothness and the velvet-tip applicator blends without \ntugging or pulling. Safe for sensitive eyes and contact lens wearers, \nophthalmologist-tested.For best results sweep the brush 4 times:Apply base color. Sweep shade on lid. Contour crease and blend. Line around eye.','https://d3t32hsnjxo7q6.cloudfront.net/i/c924006882e8e313d445a3a5394e4729_ra,w158,h184_pa,w158,h184.png','Audacious Asphalt','#515552');
+insert into [Products] ([ProductID],[API_ID],[ProductName],[Brand],[Inventory],[Price], [Description],[Image],[ColourName],[HexValue]) 
+values 
+    (,97,
+    'Maybelline Color Sensational Vivid Matte Liquid Lip Colour',
+    'maybelline',
+    20,
+    12.99, 
+    'Bold, vivid color glides easily onto lips for a velvety matte finish with Maybelline Color Sensational Vivid Matte Liquid Lip Colour. \nFormula is comfortable and creamy with a smooth feel upon application. \nWith full color coverage, the result is a rich, intense lip look in a \nrange of colors from Nude Flush to Electric Pink to Vivid Violet.Features: Lip color glides on smooth to a matte finishInfused with pure pigments for high-impact colorCreamy liquid base for a soft, cushiony feel.',
+    'https://d3t32hsnjxo7q6.cloudfront.net/i/d0b6855d9b603a00f776de498c971b88_ra,w158,h184_pa,w158,h184.png',
+    'Nude Thrill',
+    '#AC7773');
 set identity_insert [Products] off
+
+
+set identity_insert [Products] on
+insert into [Products] ([ProductID],[API_ID],[ProductName],[Brand],[Inventory],[Price], [Description],[Image],[ColourName],[HexValue]) 
+values 
+    (,97,
+    'Maybelline Color Sensational Vivid Matte Liquid Lip Colour',
+    'maybelline',
+    20,
+    12.99, 
+    'Bold, vivid color glides easily onto lips for a velvety matte finish with Maybelline Color Sensational Vivid Matte Liquid Lip Colour. \nFormula is comfortable and creamy with a smooth feel upon application. \nWith full color coverage, the result is a rich, intense lip look in a \nrange of colors from Nude Flush to Electric Pink to Vivid Violet.Features: Lip color glides on smooth to a matte finishInfused with pure pigments for high-impact colorCreamy liquid base for a soft, cushiony feel.',
+    'https://d3t32hsnjxo7q6.cloudfront.net/i/d0b6855d9b603a00f776de498c971b88_ra,w158,h184_pa,w158,h184.png',
+    'Nude Flush',
+    '#BD7A80');
+set identity_insert [Products] off
+
+set identity_insert [Products] on
+insert into [Products] ([ProductID],[API_ID],[ProductName],[Brand],[Inventory],[Price], [Description],[Image],[ColourName],[HexValue]) 
+values 
+    (,97,
+    'Maybelline Color Sensational Vivid Matte Liquid Lip Colour',
+    'maybelline',
+    20,
+    12.99, 
+    'Bold, vivid color glides easily onto lips for a velvety matte finish with Maybelline Color Sensational Vivid Matte Liquid Lip Colour. \nFormula is comfortable and creamy with a smooth feel upon application. \nWith full color coverage, the result is a rich, intense lip look in a \nrange of colors from Nude Flush to Electric Pink to Vivid Violet.Features: Lip color glides on smooth to a matte finishInfused with pure pigments for high-impact colorCreamy liquid base for a soft, cushiony feel.',
+    'https://d3t32hsnjxo7q6.cloudfront.net/i/d0b6855d9b603a00f776de498c971b88_ra,w158,h184_pa,w158,h184.png',
+    'Pink Charge',
+    '#D65F84');
+set identity_insert [Products] off
+
+set identity_insert [Products] on
+insert into [Products] ([ProductID],[API_ID],[ProductName],[Brand],[Inventory],[Price], [Description],[Image],[ColourName],[HexValue]) 
+values 
+    (,97,
+    'Maybelline Color Sensational Vivid Matte Liquid Lip Colour',
+    'maybelline',
+    20,
+    12.99, 
+    'Bold, vivid color glides easily onto lips for a velvety matte finish with Maybelline Color Sensational Vivid Matte Liquid Lip Colour. \nFormula is comfortable and creamy with a smooth feel upon application. \nWith full color coverage, the result is a rich, intense lip look in a \nrange of colors from Nude Flush to Electric Pink to Vivid Violet.Features: Lip color glides on smooth to a matte finishInfused with pure pigments for high-impact colorCreamy liquid base for a soft, cushiony feel.',
+    'https://d3t32hsnjxo7q6.cloudfront.net/i/d0b6855d9b603a00f776de498c971b88_ra,w158,h184_pa,w158,h184.png',
+    'Electric Pink',
+    '#D73D8C');
+set identity_insert [Products] off
+
