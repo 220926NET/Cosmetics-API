0a633c8cd4ca209092fca2726c32fc114e60624b
Denis Espino <despino6160.com>
2022-12-05 17:36:56 -0500

    fully cover CosmeticContext in testing

---
+++ ECommerce.API/ECommerce.Tests/Data/CosmeticsContextTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Moq;
+using Data;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerce.Tests.Repositories;
+
+public class CosmeticsContextTests
+{
+    [Fact]
+    public void Create()
+    {
+        CosmeticsContext cosmeticsContext = new CosmeticsContext();
+        Assert.NotNull(cosmeticsContext);
+    }
+}
